be66d921862b41ef02c74fd568d803c6c89b40aa
André Dias <andre25dias@gmail.com>
2024-01-10 16:33:55 -0300

    Opção de sair

--- obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88cb3bffb099551b135f9b92d6fc7b7fd2e37ce0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f344f84c12dfd0d9a320896e22ea6b2c10a6fb74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
